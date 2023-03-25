90cc6c7460d0d59c4acf358876644c9a34bf2d43
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-25 08:35:59 +0000

    add all planets

--- src/planets.ts
+++ src/planets.ts
@@ -1,7 +1,31 @@
 import { circularOrbit, SimpleBody } from './body';
 
 // Sun
-new SimpleBody(1, 0xfcf803, circularOrbit(0));
+new SimpleBody(10, 0xfcf803, circularOrbit(0.01));
+
+// Mercury
+new SimpleBody(1/6, 0x6a838f, circularOrbit(13.5));
+
+// Venus
+new SimpleBody(0.45, 0xa39679, circularOrbit(16.7));
 
 // Earth
-new SimpleBody(0.5, 0x4fc253, circularOrbit(2));
+new SimpleBody(0.5, 0x4fc253, circularOrbit(19.3));
+
+// Mars
+new SimpleBody(0.25, 0xe08358, circularOrbit(24.2));
+
+// Jupiter
+new SimpleBody(5.5, 0xf0c7b4, circularOrbit(58.4));
+
+// Saturn
+new SimpleBody(4.5, 0xba8616, circularOrbit(98.9));
+
+// Uranus
+new SimpleBody(2, 0x6a8a7e, circularOrbit(189));
+
+// Neptune
+new SimpleBody(1.5, 0x3a4dde, circularOrbit(298));
+
+// Pluto
+new SimpleBody(1/12, 0xbac9cf, circularOrbit(377));
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,4 @@
-import { PerspectiveCamera, Scene, WebGLRenderer} from 'three';
+import {PerspectiveCamera, Raycaster, Scene, Vector2, WebGLRenderer} from 'three';
 import Body from './body';
 import './planets';
 import './style.css';
@@ -11,7 +11,7 @@ let camera: PerspectiveCamera;
 function refreshUI() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 5;
+
 }
 window.addEventListener('resize', refreshUI);
 refreshUI();
@@ -19,20 +19,27 @@ refreshUI();
 renderRoot.classList.add('renderRoot');
 document.body.appendChild(renderRoot);
 
-Body.renderAll(scene);
+Body.renderAll(scene, camera);
+Body.bodies[0].focus();
 
 console.log('ready!');
 
+const raycaster = new Raycaster();
+const pointer = new Vector2();
+
 let iteration = 0;
 let start: number = Date.now();
-function animate() {
-  Body.animateAll(iteration, (Date.now() - start) / 1000);
+let click = false;
+function render() {
+  raycaster.setFromCamera(pointer, camera);
+  Body.animateAll(iteration, (Date.now() - start) / 1000, raycaster, click);
+  click = false;
   iteration += 1;
-  requestAnimationFrame( animate );
+  requestAnimationFrame( render );
   renderer.render( scene, camera );
 }
 
-animate();
+render();
 
 document.addEventListener('wheel', (e: WheelEvent) => {
   camera.position.z += e.deltaY / 500;
@@ -49,10 +56,23 @@ document.addEventListener('mouseup', () => {
   clickLast = null;
 });
 
+function updatePointer(e: MouseEvent) {
+  // calculate pointer position in normalized device coordinates
+  // (-1 to +1) for both components
+  pointer.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+  pointer.y = ( e.clientY / window.innerHeight ) * 2 -1;
+}
+
 document.addEventListener('mousemove', (e: MouseEvent) => {
+  updatePointer(e);
   if (clickLast) {
-    scene.rotation.y += (e.x - clickLast.x) / 100;
-    scene.rotation.x += (e.y - clickLast.y) / 100;
+    scene.rotation.y += (e.clientX - clickLast.x) / 100;
+    scene.rotation.x += (e.clientY - clickLast.y) / 100;
     update(e);
   }
+});
+
+renderRoot.addEventListener('click', (e: MouseEvent) => {
+  updatePointer(e);
+  click = true;
 });
