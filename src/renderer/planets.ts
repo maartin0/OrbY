5a83f2e94ede9c11a4420aba3369c14fb94a1584
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-27 18:34:10 +0100

    Flatten Body subclass and add displayname property

--- src/renderer/planets.ts
+++ src/renderer/planets.ts
@@ -1,31 +1,12 @@
-import { circularOrbit, SimpleBody } from './body';
+import Body, { circularOrbit } from './body';
 
-// Sun
-new SimpleBody(50, 0xfcf803, circularOrbit(0.01)).focused = true;
-
-// Mercury
-new SimpleBody(1/6, 0x6a838f, circularOrbit(50));
-
-// Venus
-new SimpleBody(0.45, 0xa39679, circularOrbit(53.2));
-
-// Earth
-new SimpleBody(0.5, 0x4fc253, circularOrbit(55.8));
-
-// Mars
-new SimpleBody(0.25, 0xe08358, circularOrbit(60.7));
-
-// Jupiter
-new SimpleBody(5.5, 0xf0c7b4, circularOrbit(94.9));
-
-// Saturn
-new SimpleBody(4.5, 0xba8616, circularOrbit(135.4));
-
-// Uranus
-new SimpleBody(2, 0x6a8a7e, circularOrbit(225.5));
-
-// Neptune
-new SimpleBody(1.5, 0x3a4dde, circularOrbit(334.5));
-
-// Pluto
-new SimpleBody(1/12, 0xbac9cf, circularOrbit(413.5));
+new Body('Sun', 50, 0xfcf803, circularOrbit(0.01)).focused = true;
+new Body('Mercury', 1/6, 0x6a838f, circularOrbit(50));
+new Body('Venus', 0.45, 0xa39679, circularOrbit(53.2));
+new Body('Earth', 0.5, 0x4fc253, circularOrbit(55.8));
+new Body('Mars', 0.25, 0xe08358, circularOrbit(60.7));
+new Body('Jupiter', 5.5, 0xf0c7b4, circularOrbit(94.9));
+new Body('Saturn', 4.5, 0xba8616, circularOrbit(135.4));
+new Body('Uranus', 2, 0x6a8a7e, circularOrbit(225.5));
+new Body('Neptune', 1.5, 0x3a4dde, circularOrbit(334.5));
+new Body('Pluto', 1/12, 0xbac9cf, circularOrbit(413.5));
