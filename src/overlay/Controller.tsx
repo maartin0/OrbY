7d1eb2387a38ea88934382fbd320d0b9f8c1f221
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-27 13:57:05 +0100

    add Controller class for state management

---
+++ src/overlay/Controller.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import AnimationState, {animationState, subscribe} from "../animationState";
+
+let component: Controller;
+
+type Props = {};
+type State = {
+    expanded: boolean;
+    animationState: AnimationState;
+}
+
+export default class Controller extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            expanded: false,
+            animationState,
+        }
+        component = this;
+    }
+    render(): JSX.Element {
+        return (
+            <div className='controller'>
+                <p>{this.state.animationState.time.label}</p>
+            </div>
+        );
+    }
+}
+
+subscribe((animationState: AnimationState) => {
+    component.setState({ animationState });
+});
