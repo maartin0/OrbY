8fcd5a28ce62ea2a4c438f32d06097f57e813389
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-27 13:22:55 +0100

    remove EarthSec type

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,8 +1,5 @@
 import {getNumberString} from './number';
 
-/** Milliseconds since the year 0 */
-export type EarthSec = bigint;
-
 const nativeLimit: bigint = BigInt(new Date(10000, 0, 0).valueOf());
 const nativeOffset: number = new Date(0, 0, 0).valueOf();
 const nativeFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat(undefined, {
@@ -10,7 +7,7 @@ const nativeFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat(undefined, 
     timeStyle: 'long'
 });
 
-function getDateStringNative(time: EarthSec): string {
+function getDateStringNative(time: bigint): string {
     return nativeFormatter.format(Number(time) + nativeOffset);
 }
 
@@ -19,19 +16,19 @@ export const yearMs: bigint = BigInt(365 * 24 * 60 * 60 * 1000);
 
 const nativeTimeFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat(undefined, { timeStyle: 'long' });
 const nativePartialFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat(undefined, { dateStyle: 'full' });
-function getDateStringFuture(time: EarthSec): string {
-    const yearCount: bigint = time / yearMs;
+function getDateStringFuture(ms: bigint): string {
+    const yearCount: bigint = ms / yearMs;
     const yearString: string = getNumberString(yearCount);
-    const dateOffset: Date = new Date(Number(time - (yearCount * yearMs)));
+    const dateOffset: Date = new Date(Number(ms - (yearCount * yearMs)));
     const replaceYear: string = dateOffset.getFullYear().toString(10);
     const timeString: string = nativeTimeFormatter.format(dateOffset);
     const partialString: string = nativePartialFormatter.format(dateOffset).replace(` ${replaceYear}`, '');
     return `${partialString}, ${timeString}, year ${yearString}`;
 }
 
-export function getDateString(time: EarthSec): string {
-    return time < nativeLimit
-        ? getDateStringNative(time)
-        : getDateStringFuture(time);
+export function getDateString(ms: bigint): string {
+    return ms < nativeLimit
+        ? getDateStringNative(ms)
+        : getDateStringFuture(ms);
 }
 
