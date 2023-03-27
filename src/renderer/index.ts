cca0fe36e245de0c7e6d4d20a512b4ed619a3a42
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-27 22:53:36 +0100

    code tidy

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -8,7 +8,7 @@ const scene: Scene = new Scene();
 const renderer: WebGLRenderer = new WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 const renderRoot: HTMLCanvasElement = renderer.domElement;
-const camera: PerspectiveCamera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+const camera: PerspectiveCamera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 const orbitControls: OrbitControls = new OrbitControls(camera, renderRoot);
 
 renderer.setClearColor(0x000000, 0);
