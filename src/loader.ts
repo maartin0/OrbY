7f2179c3c0abbe6e636fab75ef225ecae13a45d6
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-28 23:36:28 +0100

    optimise imports

--- src/loader.ts
+++ src/loader.ts
@@ -1,6 +1,3 @@
-import { Simulate } from 'react-dom/test-utils';
-import load = Simulate.load;
-
 export function remove(): void {
     const loader: HTMLDivElement = document.getElementById('loader') as HTMLDivElement;
     loader.classList.add('hide');
