8e09d2adeb221d6d3e13190fb205cdbf95d1f061
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-28 23:37:42 +0100

    move orbits into seperate file

--- src/renderer/Body.ts
+++ src/renderer/Body.ts
@@ -113,12 +113,3 @@ export default class Body {
 }
 
 export type PositionFunction = (ms: bigint) => Vector3;
-
-export const circularOrbit = (radius: number, speed?: number) => ((ms: bigint) => {
-  const angle: number = ((Number(ms) / 1000) * (speed ?? 1)) % (Math.PI * 2);
-  return new Vector3(
-    radius * Math.cos(angle),
-    0,
-    radius * Math.sin(angle)
-  );
-});
