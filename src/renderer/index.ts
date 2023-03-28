5497b54331fdbacb87f6c8cd04c546955c3ef9e6
Martin <16228305+maartin0@users.noreply.github.com>
2023-03-28 20:16:32 +0100

    update flexbox styling

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,14 +1,14 @@
 import './planets';
 import './keybinds';
 
-import { initialise, tick, update } from '../animationState';
+import { initialise, tick } from '../animationState';
 import { PerspectiveCamera, Scene, Vector2, WebGLRenderer } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export const scene: Scene = new Scene();
 export const renderer: WebGLRenderer = new WebGLRenderer();
 export const renderRoot: HTMLCanvasElement = renderer.domElement;
-export let camera: PerspectiveCamera = new PerspectiveCamera(75, 1, 1, 1000);
+export let camera: PerspectiveCamera = new PerspectiveCamera(75, 1, 1, 10000);
 export const size: Vector2 = new Vector2();
 export const orbitControls: OrbitControls = new OrbitControls(camera, renderRoot);
 
