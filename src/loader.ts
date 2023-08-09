742d69eb2b8ab5b0baa950406e3d6670fb110225
Martin <16228305+maartin0@users.noreply.github.com>
2023-08-09 11:41:00 +0100

    initial recommit

--- src/loader.ts
+++
@@ -1,5 +0,0 @@
-export function remove(): void {
-    const loader: HTMLDivElement = document.getElementById('loader') as HTMLDivElement;
-    loader.classList.add('hide');
-    setTimeout(() => loader.remove(), 500);
-}
--- src/renderer/keybinds.ts
+++
@@ -1,6 +0,0 @@
-import { moveFocus } from '../overlay/Controller';
-
-document.addEventListener('keydown', (event: KeyboardEvent) => {
-    if (event.key === 'ArrowRight') moveFocus(-1);
-    else if (event.key === 'ArrowLeft') moveFocus(1);
-})
