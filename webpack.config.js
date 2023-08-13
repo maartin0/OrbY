7a3dd8b200ad30e1cdbbc3e9f788480c5b9b5d60
Martin <16228305+maartin0@users.noreply.github.com>
2023-08-13 22:19:37 +0100

    copy public to assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 const isProduction = process.env.NODE_ENV === "production";
-
 const stylesHandler = "style-loader";
 
 module.exports = ({
